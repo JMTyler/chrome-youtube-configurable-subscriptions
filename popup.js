91d185a1b73e7fea79f6004871da3e87ace21171
Jared Tyler <jared@jaredtyler.ca>
2016-05-03 00:33:38 -0400

    Listing configured subscriptions before doing any API calls.

--- popup.js
+++ popup.js
@@ -1,5 +1,34 @@
 
+var apiKey = 'XXXXX';
+var subscriptions = [
+	{
+		label: 'VintageBeef',
+		channelId: 'UCu17Sme-KE87ca9OTzP0p7g',
+		query: 'The+Forest',
+	},
+	{
+		label: 'BdoubleO',
+		channelId: 'UClu2e7S8atp6tG2galK9hgg',
+		query: 'Crewcraft+Minecraft+Server',
+	}
+];
+
 document.addEventListener('DOMContentLoaded', function() {
+	var $li;
+	var $lstSubs = document.getElementById('lstSubscriptions');
+	for (var i = 0; i < subscriptions.length; i++) {
+		$li = document.createElement('li');
+		$li.innerHTML = '<a href="#">' + subscriptions[i].label + '</a>';
+		$lstSubs.appendChild($li);
+
+		(function() {
+			var closureI = i;
+			$li.onclick = function() {
+				console.log('loading up ' + subscriptions[closureI].label);
+			};
+		})();
+	}
+
 	var $btnTest = document.getElementById('btnTest');
 	$btnTest.onclick = function() {
 		var req = new XMLHttpRequest();
@@ -12,9 +41,9 @@ document.addEventListener('DOMContentLoaded', function() {
 			var $li;
 			var videoTitle;
 			var videoUri;
-			for (var i = 0; i < res.items.length; i++) {
-				videoTitle = res.items[i].snippet.title;
-				videoUri = 'https://www.youtube.com/watch?v=' + res.items[i].id.videoId;
+			for (var j = 0; j < res.items.length; j++) {
+				videoTitle = res.items[j].snippet.title;
+				videoUri = 'https://www.youtube.com/watch?v=' + res.items[j].id.videoId;
 
 				$li = document.createElement('li');
 				$li.innerHTML = '<a href="' + videoUri + '">' + videoTitle + '</a>';
@@ -27,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			console.error('ERROR', arguments);
 		};
 
-		req.open('GET', 'https://www.googleapis.com/youtube/v3/search?key=XXXXX&part=snippet&type=video&q=The+Forest&channelId=UCu17Sme-KE87ca9OTzP0p7g&order=date', true);
+		req.open('GET', 'https://www.googleapis.com/youtube/v3/search?key=&part=snippet&type=video&q=&channelId=&order=date', true);
 		req.setRequestHeader('Cache-Control', 'no-cache');
 		req.send(null);
 	};
