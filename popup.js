b44a2badfe2b0015f1a7e9c7529dcb903c14e8c4
Jared Tyler <jared@jaredtyler.ca>
2016-05-07 19:23:30 -0400

    Starting to use jQuery.

--- popup.js
+++ popup.js
@@ -23,36 +23,36 @@ var subscriptions = [
 
 var loadSubscriptionList = function() {
 	var $li;
-	var $lstSubs = document.getElementById('lstSubscriptions');
-	var $lstVids = document.getElementById('lstVideos');
-	var $lblStatus = document.getElementById('lblStatus');
+	var $lstSubs = $('#lstSubscriptions');
+	var $lstVids = $('#lstVideos');
+	var $lblStatus = $('#lblStatus');
 
-	$lstSubs.innerHTML = '';
-	$lstVids.innerHTML = '';
-	$lblStatus.innerHTML = '';
-	$btnBack.style.display = 'none';
+	$lstSubs.html('');
+	$lstVids.html('');
+	$lblStatus.html('');
+	$btnBack.css('display', 'none');
 
 	for (var i = 0; i < subscriptions.length; i++) {
 		(function() {
 			var sub = subscriptions[i];
 
-			$li = document.createElement('li');
-			$li.innerHTML = '<a href="#">' + sub.label + '</a>';
-			$lstSubs.appendChild($li);
+			$li = $('<li/>');
+			$li.html('<a href="#">' + sub.label + '</a>');
+			$lstSubs.append($li);
 
-			$li.onclick = function() {
+			$li.click(function() {
 				loadSubscription(sub);
-			};
+			});
 		})();
 	}
 };
 
 var loadSubscription = function(options) {
-	var $lstSubs = document.getElementById('lstSubscriptions');
-	var $lstVids = document.getElementById('lstVideos');
-	var $lblStatus = document.getElementById('lblStatus');
+	var $lstSubs = $('#lstSubscriptions');
+	var $lstVids = $('#lstVideos');
+	var $lblStatus = $('#lblStatus');
 
-	$lstSubs.innerHTML = '';
+	$lstSubs.html('');
 
 	var req = new XMLHttpRequest();
 	req.onload = function() {
@@ -60,8 +60,8 @@ var loadSubscription = function(options) {
 		var videoTitle;
 		var videoUri;
 
-		$lblStatus.innerText = 'SUCCESS: ' + options.label;
-		$btnBack.style.display = '';
+		$lblStatus.text('SUCCESS: ' + options.label);
+		$btnBack.css('display', '');
 
 		var start, end, step;
 		var res = JSON.parse(req.responseText);
@@ -84,14 +84,14 @@ var loadSubscription = function(options) {
 			videoTitle = res.items[j].snippet.title;
 			videoUri = 'https://www.youtube.com/watch?v=' + res.items[j].id.videoId;
 
-			$li = document.createElement('li');
-			$li.innerHTML = (isWatched ? '[W]' : '') + ' <a href="' + videoUri + '">' + videoTitle + '</a>';
-			$lstVids.appendChild($li);
+			$li = $('<li/>');
+			$li.html((isWatched ? '[W]' : '') + ' <a href="' + videoUri + '">' + videoTitle + '</a>');
+			$lstVids.append($li);
 		}
 	};
 
 	req.onerror = function() {
-		document.getElementById('lblStatus').innerText = 'ERROR loading ' + options.label;
+		$('#lblStatus').text('ERROR loading ' + options.label);
 		console.error('ERROR', arguments);
 	};
 
@@ -101,10 +101,10 @@ var loadSubscription = function(options) {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-	$btnBack = document.getElementById('btnBack');
-	$btnBack.onclick = function() {
+	$btnBack = $('#btnBack');
+	$btnBack.click(function() {
 		loadSubscriptionList();
-	};
+	});
 
 	loadSubscriptionList();
 });
